49d0f5ea30b3fac503034a914840e60626c6cbc8
CarlosWendel <wendeluzumak824@gmail.com>
2023-04-09 13:13:51 -0300

    projeto completo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 
 
@@ -9,4 +10,23 @@ const jump =()=>{
     },500)
 
 }
+const loop = setInterval(() =>{
+    const pipePosition = pipe.offsetLeft;
+    const pipeMario = +window.getComputedStyle(mario).bottom.replace('px','');
+
+    if(pipePosition <=120 && pipePosition >0 && pipeMario < 80){
+
+        pipe.style.animation ='none';
+        pipe.style.left =`${pipePosition}px`;
+
+        mario.style.animation ='none';
+        mario.style.bottom =`${pipeMario}px`;
+
+        mario.src='./imagens/death.png'
+        mario.style.width='75px';
+        mario.style.marginLeft='50px';
+        clearInterval(loop);
+    }
+
+},10)
 document.addEventListener("keydown", jump);
